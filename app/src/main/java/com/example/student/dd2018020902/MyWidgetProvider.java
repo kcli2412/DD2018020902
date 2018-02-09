cfc38341aaf851595eb6fd5e273568166cc0f705
kcli2412 <kcli2412@gmail.com>
2018-02-09 13:31:08 +0800

    setOnClickPendingIntent

--- app/src/main/java/com/example/student/dd2018020902/MyWidgetProvider.java
+++ app/src/main/java/com/example/student/dd2018020902/MyWidgetProvider.java
@@ -1,10 +1,15 @@
 package com.example.student.dd2018020902;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
+
+import java.util.Date;
 
 /**
  * Created by Student on 2018/2/9.
@@ -16,10 +21,20 @@ public class MyWidgetProvider extends AppWidgetProvider {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.mywidget);
+        Date date = new Date();
+        //remoteViews.setTextViewText(R.id.textView, date.toString());
+
+        Intent it = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context, 0, it, 0);
+        remoteViews.setOnClickPendingIntent(R.id.button, pendingIntent);
+
         ComponentName thisWidget = new ComponentName(context, MyWidgetProvider.class);
         // 取得 Widgets 管理器
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
         // 透過管理器來對指定的Widgets 實例進行單項更新
         manager.updateAppWidget(thisWidget, remoteViews);
     }
+
+
 }
